4ba2638046eef2f472efa729600404d5e6601c7a
kybidium <bogsredeludi@gmail.com>
2023-03-05 01:47:44 -0600

    implemented sum_type_priority, untested

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -6,6 +6,7 @@ Resources Consulted:
 f = open("input3.txt")
 f_read = f.read()
 
+# generalize somehow?
 def sum_priority(l_string):
     # generating alphabet dictionary
     alpha_dict = {}
@@ -24,5 +25,31 @@ def sum_priority(l_string):
     
     return prio_tot       
 
+def sum_type_priority(l_string):
+    # generating alphabet dictionary
+    alpha_dict = {}
+    alpha_str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for idx, let in enumerate(alpha_str):
+        alpha_dict[let] = idx + 1
+
+    prio_tot = 0
+    l_lst = l_string.split("\n")
+    for n in range(0, len(l_lst)//3, 3):
+        lst_smol = l_lst[n:n+3]
+        tally = 0
+        for let in alpha_str:
+            for s_let in lst_smol:
+                if let in s_let:
+                    tally += 1
+            if tally == 3:
+                prio_tot += alpha_dict[s_let]
+                break
+
+    return prio_tot    
+
+# generalize for groups of n elvess
+# have alpha string generation function
+# figure out less degenerate way (avoiding 3 for loops somehow)
 print(sum_priority(f_read))
+print(sum_type_priority(f_read))
 f.close()
