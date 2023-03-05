5bafdd1b3a65ca496fb471b4c27759274affd322
kybidium <bogsredeludi@gmail.com>
2023-03-05 17:47:34 -0600

    added continue statement contingent on second for loop breaking

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -4,6 +4,7 @@ f_read = f.read()
 def fully_contained(ID_lst):
     tally = 0
     for line in ID_lst.strip().split("\n"):
+        broken = False
         first, second = line.split(",")
         first1, first2 = first.split("-")
         second1, second2 = second.split("-")
@@ -11,8 +12,11 @@ def fully_contained(ID_lst):
         range2 = list(range(int(second1), int(second2) + 1))
         for n in range((min(len(range1), len(range2)))):
             if not (range2[n] in range1 or range1[n] in range2):
-                print(range2[n] in range1, range1[n] in range2)
+                broken = True
                 break
+        if broken == True:
+            broken = False
+            continue
         tally += 1
     return tally
 
