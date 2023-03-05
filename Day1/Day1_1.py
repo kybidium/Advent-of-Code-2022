6d7bdd02a86dbd192fd01db683be5ad3e5da7c1c
kybidium <bogsredeludi@gmail.com>
2023-03-04 21:19:27 -0600

    Implemented greatest_num function

--- Day1/Day1_1.py
+++ Day1/Day1_1.py
@@ -1,4 +1,12 @@
-f = open("input.txt")
+"""
+Resources Consulted:
+    https://www.freecodecamp.org/news/python-list-remove-how-to-remove-an-item-
+        from-a-list-in-python/#:~:text=The%20remove()%20method%20is,not%20by%20
+        its%20index%20number.
+
+"""
+
+f = open("input1.txt")
 f_read = f.read()
 
 def greatest_sum(group_str):
@@ -21,7 +29,37 @@ def greatest_sum(group_str):
             cal_tal = 0
     return max(cal_lst)
 
+def greatest_num(group_str, n):
+    """
+    Returns sum of the n greatest sums of a group of numbers in a string
+    containing newline-separated groups of newline-separated numbers. 
+
+    Args: 
+        group_str: a string in the described format (str)
+        n: the number of greatest sums to be added (int)
+
+    Returns: the sum of the 
+        n greatest sums of any group of numbers in the string (int)
+    """
+    # maybe decompose into helper function for producing list
+    cal_lst = []
+    cal_tal = 0
+    for num in group_str.split("\n"):
+        if num:
+            cal_tal += int(num)
+        else:
+            cal_lst.append(cal_tal)
+            cal_tal = 0
+
+    lst_maxes = []
+    for n in range(n):
+        lst_maxes.append(max(cal_lst))
+        cal_lst.remove(max(cal_lst))
+
+    return sum(lst_maxes)
+
 print(greatest_sum(f_read))
+print(greatest_num(f_read, 3))
         
 
         
