8782585023e895ca551e58d367d968bf0ac1439f
kybidium <bogsredeludi@gmail.com>
2023-03-05 00:16:36 -0600

    implemented rock_paper_scissors_calc_2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,5 +1,6 @@
 f = open("input2.txt")
 f_read = f.read().strip()
+
 def rock_paper_scissors_calculator(strat):
     dict_A = {
         "X": 3,
@@ -24,7 +25,7 @@ def rock_paper_scissors_calculator(strat):
         "Y": 2,
         "Z": 3,
     }
-    # format is letterspaceletterspacenewline
+
     score = 0
     for round in strat.split("\n"):
         # is there a better way to process the file?
@@ -33,5 +34,39 @@ def rock_paper_scissors_calculator(strat):
 
     return score
 
+def rock_paper_scissors_calc_2(strat):
+    dict_A = {
+        "X": 3,
+        "Y": 1,
+        "Z": 2,
+    }
+    dict_B = {
+        "X": 1,
+        "Y": 2,
+        "Z": 3,
+    }
+    dict_C = {
+        "X": 2,
+        "Y": 3,
+        "Z": 1,
+    }
+    dict_gen  = {
+        "A": dict_A,
+        "B": dict_B,
+        "C": dict_C,
+        "X": 0,
+        "Y": 3,
+        "Z": 6,
+    }
+
+    score = 0
+    for round in strat.split("\n"):
+        # is there a better way to process the file?
+        gain = int(dict_gen[round[0]][round[2]]) + int(dict_gen[round[2]])
+        score += gain
+
+    return score
+    # is there any way to generalize this dictionary creation/use previous?
 print(rock_paper_scissors_calculator(f_read))
+print(rock_paper_scissors_calc_2(f_read))
 f.close()
