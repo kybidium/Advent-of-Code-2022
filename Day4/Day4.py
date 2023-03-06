0fbbce71eeb6903916c9edefcfb7b4289fa63f1f
kybidium <bogsredeludi@gmail.com>
2023-03-05 18:12:18 -0600

    implemented overlaps function, removed test print statements

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -17,11 +17,24 @@ def fully_contained(ID_lst):
         second1, second2 = second.split("-")
         range1 = set(range(int(first1), int(first2) + 1))
         range2 = set(range(int(second1), int(second2) + 1))
-        print("newline")
         if (range1 & range2 == range1) or (range1 & range2 == range2):
             tally += 1
 
     return tally
 
+def overlaps(ID_lst):
+    tally = 0
+    for line in ID_lst.strip().split("\n"):
+        first, second = line.split(",")
+        first1, first2 = first.split("-")
+        second1, second2 = second.split("-")
+        range1 = set(range(int(first1), int(first2) + 1))
+        range2 = set(range(int(second1), int(second2) + 1))
+        if range1 & range2:
+            tally += 1
+
+    return tally
+
 print(fully_contained(f_read))
+print(overlaps(f_read))
 f.close()
