40ed8da8228fe6a383d09da201e0e66672154ae1
kybidium <bogsredeludi@gmail.com>
2023-03-05 18:09:17 -0600

    using set intersection logic instead of attempging with list

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -1,23 +1,25 @@
+"""
+Resources Consulted:
+    https://techbeamers.com/program-python-list-contains-elements/
+    https://www.w3schools.com/python/ref_set_intersection.asp
+    https://favtutor.com/blogs/set-intersection-python#:~:text=Difference%20betw
+        een%20set%20intersection(),structure%20to%20find%20the%20intersection.
+"""
 f = open("input4.txt")
 f_read = f.read()
 
 def fully_contained(ID_lst):
     tally = 0
     for line in ID_lst.strip().split("\n"):
-        broken = False
         first, second = line.split(",")
         first1, first2 = first.split("-")
         second1, second2 = second.split("-")
-        range1 = list(range(int(first1), int(first2) + 1))
-        range2 = list(range(int(second1), int(second2) + 1))
-        for n in range((min(len(range1), len(range2)))):
-            if not (range2[n] in range1 or range1[n] in range2):
-                broken = True
-                break
-        if broken == True:
-            broken = False
-            continue
-        tally += 1
+        range1 = set(range(int(first1), int(first2) + 1))
+        range2 = set(range(int(second1), int(second2) + 1))
+        print("newline")
+        if (range1 & range2 == range1) or (range1 & range2 == range2):
+            tally += 1
+
     return tally
 
 print(fully_contained(f_read))
