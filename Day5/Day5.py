71105d2f6d5bfffb0d9647b91cbe5cb7a4ccdefa
kybidium <bogsredeludi@gmail.com>
2023-03-05 18:46:42 -0600

    converted string into line-by-line list in process_stacks

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -3,7 +3,10 @@ f = open("input5.txt")
 f_read = f.read()
 
 def process_stacks(stack_string):
-    stack_str = stack_string[:9]
-    command_str = stack_string[10:]
-    print(stack_str, command_str)
+    stack_string_list = stack_string.split("\n")
+    stack_str = stack_string_list[:9]
+    command_str = stack_string_list[10:]
+    print(str(stack_str)+ "nyuguh", str(command_str))
+
+process_stacks(f_read)
 f.close()
