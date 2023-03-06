82da50d307c648ce9966c737f05ba3665c94e94e
kybidium <bogsredeludi@gmail.com>
2023-03-05 18:43:13 -0600

    implementing process_stacks function, added code to create separate stack and command strings

---
+++ Day5/Day5.py
@@ -0,0 +1,9 @@
+# need to generalize for n stacks of height n
+f = open("input4.txt")
+f_read = f.read()
+
+def process_stacks(stack_string):
+    stack_str = stack_string[:9]
+    command_str = stack_string[10:]
+    print(stack_str, command_str)
+f.close()
