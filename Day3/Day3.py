252ef32e8873ce74df48c29620f5f69ef86ca98a
kybidium <bogsredeludi@gmail.com>
2023-03-07 01:45:21 -0600

    Adding Day3.py

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -8,13 +8,21 @@ Resources Consulted:
 f = open("input3.txt")
 f_read = f.read()
 
-# generalize somehow?
-def sum_priority(l_string):
-    # generating alphabet dictionary
+def alpha_gen():
+    """
+    """
+    # generating alphabet-to-value dictionary
     alpha_dict = {}
     alpha_str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for idx, let in enumerate(alpha_str):
         alpha_dict[let] = idx + 1
+    return alpha_dict
+
+def sum_priority(l_string):
+    """
+    """
+    # create alphabet-to-value dictionary
+    alpha_dict = alpha_gen()
 
     prio_tot = 0
     l_lst = l_string.split("\n")
@@ -27,23 +35,23 @@ def sum_priority(l_string):
     
     return prio_tot       
 
-def sum_type_priority(l_string):
-    # generating alphabet dictionary
-    alpha_dict = {}
-    alpha_str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    for idx, let in enumerate(alpha_str):
-        alpha_dict[let] = idx + 1
+def sum_type_priority(l_string, n2):
+    """
+    
+    """
+    # create alphabet-to-value dictionary
+    alpha_dict = alpha_gen()
 
     prio_tot = 0
     l_lst = l_string.split("\n")
-    for n in range(0, len(l_lst), 3):
-        lst_smol = l_lst[n:n+3]
-        for let in alpha_str:
+    for n in range(0, len(l_lst), n2):
+        lst_smol = l_lst[n:n+n2]
+        for let in list(alpha_dict):
             tally = 0
             for s_let in lst_smol:
                 if let in s_let:
                     tally += 1
-            if tally == 3:
+            if tally == n2:
                 prio_tot += alpha_dict[let]
                 broken = True
                 tally = 0
@@ -55,5 +63,5 @@ def sum_type_priority(l_string):
 # have alpha string generation function
 # figure out less degenerate way (avoiding 3 for loops somehow)
 print(sum_priority(f_read))
-print(sum_type_priority(f_read))
+print(sum_type_priority(f_read, 3))
 f.close()
